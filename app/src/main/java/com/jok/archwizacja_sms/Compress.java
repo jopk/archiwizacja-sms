f0d6789806d3184306a62007d60e94d3b36733a7
dahnik <dahn000@gmail.com>
2015-05-30 20:34:12 +0200

    changes in zip & unzip

--- app/src/main/java/com/jok/archwizacja_sms/Compress.java
+++ app/src/main/java/com/jok/archwizacja_sms/Compress.java
@@ -2,7 +2,6 @@ package com.jok.archwizacja_sms;
 
 import android.content.Context;
 import android.os.Environment;
-import android.util.Log;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -25,26 +24,22 @@ public class Compress {
     private static final int BUFFER = 1024;
     private String ZIPFILE = Environment.getExternalStorageDirectory().getAbsolutePath() + "/compress/" + "compressedsms.zip";
     private String FILEPATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/compress/";
+    private Context ctx;
 
-
-    public Compress() {}
+    public Compress(Context ctx) { this.ctx = ctx;}
 
     private void deleteFiles(){
-        File folder = new File(FILEPATH);
-        if (folder.exists()){
-            if(folder.isDirectory()){
-                String[] files = folder.list();
-                for(int i=0;i<files.length;i++){
-                    if(files[i].endsWith(".xml")){
-                        new File(folder,files[i]).delete();
-                    }
-                }
+        File folder = ctx.getFilesDir();
+        String[] files = folder.list();
+        for(int i=0;i<files.length;i++){
+            if(files[i].endsWith(".xml")){
+                new File(folder,files[i]).delete();
             }
         }
+
     }
 
-    public String[] writeFiles(String[] data, int amount, String tag, Context ctx) {
-        //File dir = ctx.getFilesDir();
+    public String[] writeFiles(String[] data, int amount, String tag) {
         String[] files = new String[data.length];
         for (int i = 0; i < data.length; i++) {
             try {
@@ -53,11 +48,6 @@ public class Compress {
                 FileOutputStream fos = ctx.openFileOutput(files[i],Context.MODE_PRIVATE);
                 fos.write(data[i].getBytes());
                 fos.close();
-                /*File file = new File(dir, files[i]);
-                PrintWriter pw = new PrintWriter(file);
-                pw.write(data[i]);
-                pw.flush();
-                pw.close();*/
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -90,20 +80,14 @@ public class Compress {
         return files;
     }
 
-    private final String TAG = "Compress";
-
     public String[] readFiles(Context ctx) {
         File path = ctx.getFilesDir();
         File fileslist[] = path.listFiles();
         String[] files = new String[fileslist.length];
-        Log.wtf(TAG, "" + files.length);
-        byte[] buffer = new byte[1024];
         for (int i=0;i<files.length;i++) {
             try {
                 String name = fileslist[i].getName();
-                Log.wtf(TAG, name);
                 if (name.endsWith(".xml")) {
-                    //String file = fileslist[i].getName();
                     FileInputStream fis = ctx.openFileInput(name);
                     int ch;
                     StringBuilder sb = new StringBuilder();
@@ -112,16 +96,6 @@ public class Compress {
                     }
                     files[i] = sb.toString();
                     fis.close();
-                    /*
-                    InputStreamReader isr = new InputStreamReader(fis);
-                    BufferedReader bufferedReader = new BufferedReader(isr);
-                    StringBuilder sb = new StringBuilder();
-                    String line;
-                    while((line = bufferedReader.readLine())!=null){
-                        sb.append(line);
-                    }
-                    files[i] = sb.toString();*/
-
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -159,14 +133,15 @@ public class Compress {
     }
 
     public void zip(String[] files) {
-        File filepath = new File(FILEPATH);
-        File compFile = new File(ZIPFILE);
+        //File filepath = new File(FILEPATH);
+        File compFile = new File("compressedsms.zip");
 
-        if (!filepath.exists())
+        /*if (!filepath.exists())
             filepath.mkdir();
-
+        */
         if(compFile.exists()) {
             unzip();
+            File filepath = ctx.getFilesDir();
             files = filepath.list(new FilenameFilter() {
                 @Override
                 public boolean accept(File dir, String filename) {
@@ -176,12 +151,12 @@ public class Compress {
         }
         try {
             BufferedInputStream origin = null;
-            FileOutputStream dest = new FileOutputStream(ZIPFILE);
+            FileOutputStream dest = ctx.openFileOutput("compressedsms.zip",Context.MODE_PRIVATE);
             ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
 
             byte data[] = new byte[BUFFER];
             for(String file : files) {
-                file = Environment.getExternalStorageDirectory().getAbsolutePath() + "/compress/" + file;
+                //file = Environment.getExternalStorageDirectory().getAbsolutePath() + "/compress/" + file;
                 FileInputStream fi = new FileInputStream(file);
                 origin = new BufferedInputStream(fi, BUFFER);
                 ZipEntry entry = new ZipEntry(file.substring(file.lastIndexOf("/") + 1));
@@ -196,7 +171,7 @@ public class Compress {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        //deleteFiles();
+        deleteFiles();
     }
 
 
@@ -205,7 +180,7 @@ public class Compress {
         ZipInputStream zis;
         try {
             String filename;
-            fis = new FileInputStream(ZIPFILE);
+            fis = ctx.openFileInput("compressedsms.zip");
             zis = new ZipInputStream(new BufferedInputStream(fis));
             ZipEntry zipEntry;
             byte[] data = new byte[BUFFER];
@@ -216,13 +191,13 @@ public class Compress {
 
                 // Need to create directories if not exists, or
                 // it will generate an Exception...
-                if (zipEntry.isDirectory()) {
+               /* if (zipEntry.isDirectory()) {
                     File fmd = new File(FILEPATH + filename);
                     fmd.mkdirs();
                     continue;
-                }
+                }*/
 
-                FileOutputStream fos = new FileOutputStream(FILEPATH + filename);
+                FileOutputStream fos = ctx.openFileOutput(filename, Context.MODE_PRIVATE);
 
                 while ((count = zis.read(data)) != -1) {
                     fos.write(data, 0, count);
